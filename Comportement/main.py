751662aa1e7bd6f1709cbc4786f1d632e4deff39
gueryacine <gueryacine@hotmail.com>
2017-05-24 09:57:49 +0200

    Updated I2C Code for Arduino

---
+++ Comportement/main.py
@@ -0,0 +1,24 @@
+from math import *
+
+class Canette:
+    def __init__(self, url):
+        self.posX = 250
+        self.posY = 250
+
+
+
+class Deplacement:
+    def __init__(self,):
+        self.countDetection = 0
+        self.ListPos =[]
+
+
+
+
+
+
+
+def angle(x,y):
+    return degrees(atan2(x,y))
+    
+        
