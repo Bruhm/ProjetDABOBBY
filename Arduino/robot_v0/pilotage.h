bb394a0c551853bce53d60e927eb83fada819a85
PikouZ31 <mattskull.ma@gmail.com>
2017-05-25 15:23:54 +0200

    Update pilotage.h
    add magnet

--- Arduino/robot_v0/pilotage.h
+++ Arduino/robot_v0/pilotage.h
@@ -2,6 +2,7 @@
 #define PILOTAGE_H
 
 #include "Arduino.h"
+#include <Servo.h> 
 
 static const int M11 = 12;
 static const int M12 = 11;
@@ -9,7 +10,13 @@ static const int M22 = 7;
 static const int M21 = 4;
 static const int go = 3;
 static const int go2 = 6;
+static const int servoPin = 9; 
+static const int Electromagnet = 8;
+static const Servo Servo1; 
 
+void INIT_MAGNET();
+void MAGNET_OFF();
+void MAGNET_ON();
 void INIT_MOTORS_PINS();
 void ALL_MOTORS_ON();
 void ALL_MOTORS_OFF();
