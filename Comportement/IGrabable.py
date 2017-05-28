2ec760505a33a33c7cbbc52c40701eb2ae499642
gueryacine <gueryacine@hotmail.com>
2017-05-28 04:44:00 +0200

    Update IGrabable.py

--- Comportement/IGrabable.py
+++ Comportement/IGrabable.py
@@ -2,6 +2,9 @@ from IDetectable import IDetectable
 
 class IGrabable:
     def __init__(self, surface=0, angle=0):
-        IDetectable.__init__(self, surface, angle)
-        self.isGrabable = True
+        self.IDetectable = IDetectable(surface, angle)
+        self.isGrabableVar = True
+    def isGrabable(self):
+        return self.isGrabableVar
+
         
